879d60d52a999406577fd1749353ae11b2decd46
Alberto Gonzalez <alberto.gonzalez@uji.es>
2018-06-04 22:59:19 +0200

    Components and view logic created

--- InkCalculator/MainWindow.xaml.cs
+++ InkCalculator/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace InkCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Mode InputMode
+        {
+            get => _inputMode;
+            private set {
+                _inputMode = value;
+                if (_inputMode == Mode.PAUSED)
+                {
+                    PausedModeBtn.IsEnabled = false;
+                    FluidModeBtn.IsEnabled = true;
+
+                    PausedModeCtrl.Visibility = Visibility.Visible;
+                    FluidModeCtrl.Visibility = Visibility.Collapsed;
+                } else
+                {
+                    PausedModeBtn.IsEnabled = true;
+                    FluidModeBtn.IsEnabled = false;
+
+                    PausedModeCtrl.Visibility = Visibility.Collapsed;
+                    FluidModeCtrl.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private Mode _inputMode;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Clean_Click(object sender, RoutedEventArgs e)
+        {
+            ClearResultsArea();
+            ClearExpressionsArea();
+        }
+
+        private void DeleteLast_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PausedMode_Click(object sender, RoutedEventArgs e)
+        {
+            InputMode = Mode.PAUSED;
+        }
+
+        private void FluidMode_Click(object sender, RoutedEventArgs e)
+        {
+            InputMode = Mode.FLUID;
+        }
+
+        private void ClearResultsArea()
+        {
+            ResultsArea.Text = "0";
+        }
+
+        private void ClearExpressionsArea()
+        {
+            ExpressionsArea.Strokes.Clear();
+        }
+
+        public enum Mode
+        {
+            PAUSED, FLUID
         }
     }
 }
